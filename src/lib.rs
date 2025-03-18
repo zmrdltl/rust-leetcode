dadcf889f677941fba1a454d66554165f8dfafba
john <john@cluml.com>
2025-03-18 16:44:04 +0900

    Add vectors module and numbers mod.rs
    - Add vectors module with mod.rs for
      vector-related solutions
    - Add numbers mod.rs to complete module structure
    - Update lib.rs to include new modules and exports
    
    Signed-off-by: john <john@cluml.com>
    
    src/numbers/mod.rs #	new file:
    src/vectors/concatenation_array.rs #	new file:
    src/vectors/mod.rs #

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod numbers;
+pub mod vectors;
 
 pub use numbers::*;
+pub use vectors::*;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const REPOSITORY: &str = "https://github.com/zmrdltl/rust-leetcode";
